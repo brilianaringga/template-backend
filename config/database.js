ab1b4b732258896e891b5ee0dcbde591cb5cace0
brilian aringga <brilianaringga@gmail.com>
2021-01-12 10:20:38 +0700

    Add database schema options to database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,14 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(
+  process.env.DATABASE_DB,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    schema: process.env.DATABASE_SCHEMA,
+    host: process.env.DATABASE_HOST,
+    dialect: "postgres",
+  }
+);
 
 sequelize
   .authenticate()
@@ -11,7 +20,17 @@ sequelize
   });
 
 if (process.env.NODE_ENV == "dev") {
-  sequelize.sync({ force: true });
+  try {
+    // sequelize.query(`
+    //     CREATE SCHEMA IF NOT EXISTS ${process.env.DATABASE_SCHEMA};
+    // `);
+    // sequelize.query(`
+    //     SET SCHEMA ${process.env.DATABASE_SCHEMA};
+    // `);
+    sequelize.sync({ force: true });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = sequelize;
